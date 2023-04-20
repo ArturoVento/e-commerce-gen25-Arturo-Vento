83593fc03008fdd34ec97aa1c990c3b2b87278ac
Arturo Vento <arthurvnto@hotmail.com>
2023-04-19 23:13:09 -0500

    Proyecto Completo

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+import cart from './components/cart.js'
+import loader from './components/loader.js'
+import darkTheme from './components/darkTheme.js'
+import products from './components/products.js'
+import showCart from './components/showCart.js'
+import showMenu from './components/showMenu.js'
+import getProducts from './helpers/getProducts.js'
+
+/* UI Element */
+
+darkTheme()
+
+/* Ocultar loader */
+loader()
+
+/* Mostrar Menu */
+showMenu()
+
+/* Mostrar Carrito */
+showCart()
+
+/* End UI Element */
+
+/* Products */
+const { db, printProducts } = products(await getProducts())
+
+/* Carrito */
+cart(db, printProducts)
